40219035fa96abc5cd3486ea711d65d9c8816b9d
adeelikram <rahil.ikram67@gmail.com>
2022-05-21 13:10:14 +0500

    First

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+import styles from './App.module.css';
+const App = () => {
+  return (
+    <div class={styles.App}>
+      <img src="./assets/favicon.ico" alt="" />
+    </div>
+  );
+};
+
+export default App;
